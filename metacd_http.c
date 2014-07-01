c957e888d1c1ce68e11af4c0bbb0442880219119
jfsmig <jfsmig@localhost>
2014-07-01 16:19:34 +0200

    m2v2 features added
    PURGE, DEDUP, OPEN, CLOSE, CREATE, DESTROY, HAS

--- metacd_http.c
+++ metacd_http.c
@@ -439,170 +439,6 @@ action_dir_status(struct http_request_s *rq, struct http_reply_ctx_s *rp,
 
 // META2 handler ---------------------------------------------------------------
 
-static GError*
-_extract_m2_url(const gchar *uri, struct hc_url_s **rurl)
-{
-	struct hc_url_s *url = hc_url_empty();
-
-	GError* _on_ns(const gchar *v) {
-		hc_url_set(url, HCURL_NS, *v ? v : NULL);
-		return NULL;
-	}
-	GError* _on_ref(const gchar *v) {
-		hc_url_set(url, HCURL_REFERENCE, *v ? v : NULL);
-		return NULL;
-	}
-	GError* _on_path(const gchar *v) {
-		hc_url_set(url, HCURL_PATH, *v ? v : NULL);
-		return NULL;
-	}
-	GError* _on_version(const gchar *v) {
-		hc_url_set(url, HCURL_VERSION, *v ? v : NULL);
-		return NULL;
-	}
-	GError* _on_size(const gchar *v) {
-		hc_url_set_option(url, "size", *v ? v : NULL);
-		return NULL;
-	}
-	GError* _on_policy(const gchar *v) {
-		hc_url_set_option(url, "policy", *v ? v : NULL);
-		return NULL;
-	}
-
-	struct url_action_s actions[] = {
-		{"ns", _on_ns},
-		{"ref", _on_ref},
-		{"path", _on_path},
-		{"version", _on_version},
-		{"size", _on_size},
-		{"policy", _on_policy},
-		{NULL, NULL}
-	};
-	GError *e = _split_and_parse_url(uri, actions);
-	if (e) {
-		hc_url_clean(url);
-		return e;
-	}
-	else {
-		*rurl = url;
-		return NULL;
-	}
-}
-
-static GError*
-_single_get_v1(const gchar *m2v, struct hc_url_s *url, GSList **beans)
-{
-	int rc;
-	GError *err = NULL;
-	struct metacnx_ctx_s cnx;
-
-	struct meta1_service_url_s *m2 = meta1_unpack_url(m2v);
-	metacnx_clear(&cnx);
-	rc = metacnx_init_with_url(&cnx, m2->host, &err);
-	meta1_service_url_clean(m2);
-
-	if (!rc) {
-		g_prefix_error(&err, "URL error: ");
-		return err;
-	}
-
-	struct meta2_raw_content_v2_s *raw = NULL;
-	rc = meta2_remote_stat_content_v2(&cnx, hc_url_get_id(url),
-			hc_url_get(url, HCURL_PATH), &raw, &err);
-	metacnx_close(&cnx);
-	metacnx_clear(&cnx);
-
-	if (!rc) {
-		g_prefix_error(&err, "M2V1 error: ");
-		return err;
-	}
-
-	*beans = m2v2_beans_from_raw_content_v2("fake", raw);
-	meta2_raw_content_v2_clean(raw);
-	return NULL;
-}
-
-static GError*
-_multiple_list_v2(gchar **m2v, struct hc_url_s *url, GSList **beans)
-{
-	for (; m2v && *m2v ;++m2v) {
-		struct meta1_service_url_s *m2 = meta1_unpack_url(*m2v);
-		GError *err = m2v2_remote_execute_LIST(m2->host, NULL, url, 0, beans);
-		meta1_service_url_clean(m2);
-
-		if (!err) // M2V2 success
-			return NULL;
-
-		GRID_INFO("M2V2 error : (%d) %s", err->code, err->message);
-		g_prefix_error(&err, "M2V2 error: ");
-
-		if (err->code >= 400)
-			return err;
-		g_clear_error(&err);
-	}
-
-	return NEWERROR(500, "No META2 replied");
-}
-
-static GError*
-_multiple_beans_v2(gchar **m2v, struct hc_url_s *url, GSList **beans)
-{
-	gchar *end = NULL;
-	const gchar *strpol = hc_url_get_option_value(url, "policy");
-	const gchar *strsize = hc_url_get_option_value(url, "size");
-	gint64 size = g_ascii_strtoll(strsize, &end, 10);
-
-	for (; m2v && *m2v ;++m2v) {
-		struct meta1_service_url_s *m2 = meta1_unpack_url(*m2v);
-		GError *err = m2v2_remote_execute_BEANS(m2->host, NULL, url,
-				strpol, size, 0, beans);
-		meta1_service_url_clean(m2);
-
-		if (!err) // M2V2 success
-			return NULL;
-
-		GRID_INFO("M2V2 error : (%d) %s", err->code, err->message);
-		g_prefix_error(&err, "M2V2 error: ");
-
-		if (err->code >= 400)
-			return err;
-		g_clear_error(&err);
-	}
-
-	return NEWERROR(500, "No META2 replied");
-}
-
-static GError*
-_multiple_get_v2_or_v1(gchar **m2v, struct hc_url_s *url, GSList **beans)
-{
-	for (; m2v && *m2v ;++m2v) {
-		struct meta1_service_url_s *m2 = meta1_unpack_url(*m2v);
-		GError *err = m2v2_remote_execute_GET(m2->host, NULL, url, 0, beans);
-		meta1_service_url_clean(m2);
-
-		if (!err) // M2V2 success
-			return NULL;
-
-		if (err->code == 404) {
-			GRID_INFO("M2V2 error : probably a M2V1");
-			g_clear_error(&err);
-			err = _single_get_v1(*m2v, url, beans);
-		}
-		else {
-			GRID_INFO("M2V2 error : (%d) %s", err->code, err->message);
-			g_prefix_error(&err, "M2V2 error: ");
-		}
-
-		if (!err) // M2V1 success
-			return NULL;
-		if (err->code >= 400)
-			return err;
-		g_clear_error(&err);
-	}
-
-	return NEWERROR(500, "No META2 replied");
-}
-
 static void
 _json_BEAN_only(GString *gstr, GSList *l, gconstpointer selector,
 		void (*code)(GString*,gpointer))
@@ -698,9 +534,64 @@ _json_dump_all_beans(GString *gstr, struct hc_url_s *url, GSList *beans)
 	g_string_append_len(gstr, "]}", 2);
 }
 
+static GError*
+_extract_m2_url(const gchar *uri, struct hc_url_s **rurl)
+{
+	struct hc_url_s *url = hc_url_empty();
+
+	GError* _on_ns(const gchar *v) {
+		hc_url_set(url, HCURL_NS, *v ? v : NULL);
+		return NULL;
+	}
+	GError* _on_ref(const gchar *v) {
+		hc_url_set(url, HCURL_REFERENCE, *v ? v : NULL);
+		return NULL;
+	}
+	GError* _on_path(const gchar *v) {
+		hc_url_set(url, HCURL_PATH, *v ? v : NULL);
+		return NULL;
+	}
+	GError* _on_version(const gchar *v) {
+		hc_url_set(url, HCURL_VERSION, *v ? v : NULL);
+		return NULL;
+	}
+	GError* _on_size(const gchar *v) {
+		hc_url_set_option(url, "size", *v ? v : NULL);
+		return NULL;
+	}
+	GError* _on_stgpol(const gchar *v) {
+		hc_url_set_option(url, "stgpol", *v ? v : NULL);
+		return NULL;
+	}
+	GError* _on_verpol(const gchar *v) {
+		hc_url_set_option(url, "verpol", *v ? v : NULL);
+		return NULL;
+	}
+
+	struct url_action_s actions[] = {
+		{"ns", _on_ns},
+		{"ref", _on_ref},
+		{"path", _on_path},
+		{"version", _on_version},
+		{"size", _on_size},
+		{"stgpol", _on_stgpol},
+		{"verpol", _on_verpol},
+		{NULL, NULL}
+	};
+	GError *e = _split_and_parse_url(uri, actions);
+	if (e) {
+		hc_url_clean(url);
+		return e;
+	}
+	else {
+		*rurl = url;
+		return NULL;
+	}
+}
+
 static GError *
-_resolve_m2_and_get(struct hc_resolver_s *r, struct hc_url_s *u,
-		GSList **beans)
+_resolve_m2_and_do(struct hc_resolver_s *r, struct hc_url_s *u,
+		GError* (*hook) (struct meta1_service_url_s *m2))
 {
 	gchar **m2v = NULL;
 	GError *err;
@@ -712,53 +603,59 @@ _resolve_m2_and_get(struct hc_resolver_s *r, struct hc_url_s *u,
 
 	if (!*m2v)
 		err = NEWERROR(CODE_CONTAINER_NOTFOUND, "No meta2 located");
-	else
-		err =  _multiple_get_v2_or_v1(m2v, u, beans);
+	else {
+		for (; m2v && *m2v ;++m2v) {
+			struct meta1_service_url_s *m2 = meta1_unpack_url(*m2v);
+			err = hook(m2);
+			meta1_service_url_clean(m2);
 
+			if (!err)
+				goto exit;
+
+			GRID_INFO("M2V2 error : (%d) %s", err->code, err->message);
+			g_prefix_error(&err, "M2V2 error: ");
+
+			if (err->code >= 400)
+				goto exit;
+			g_clear_error(&err);
+		}
+		if (!err)
+			err = NEWERROR(500, "No META2 replied");
+	}
+exit:
 	g_strfreev(m2v);
 	return err;
 }
 
-static GError *
-_resolve_m2_and_beans(struct hc_resolver_s *r, struct hc_url_s *u,
-		GSList **beans)
+static GError*
+_single_get_v1(struct meta1_service_url_s *m2, struct hc_url_s *url, GSList **beans)
 {
-	gchar **m2v = NULL;
-	GError *err;
+	int rc;
+	GError *err = NULL;
+	struct metacnx_ctx_s cnx;
 
-	if (NULL != (err = hc_resolve_reference_service(r, u, "meta2", &m2v))) {
-		g_prefix_error(&err, "Resolution error: ");
+	metacnx_clear(&cnx);
+	rc = metacnx_init_with_url(&cnx, m2->host, &err);
+
+	if (!rc) {
+		g_prefix_error(&err, "URL error: ");
 		return err;
 	}
 
-	if (!*m2v)
-		err = NEWERROR(CODE_CONTAINER_NOTFOUND, "No meta2 located");
-	else
-		err =  _multiple_beans_v2(m2v, u, beans);
+	struct meta2_raw_content_v2_s *raw = NULL;
+	rc = meta2_remote_stat_content_v2(&cnx, hc_url_get_id(url),
+			hc_url_get(url, HCURL_PATH), &raw, &err);
+	metacnx_close(&cnx);
+	metacnx_clear(&cnx);
 
-	g_strfreev(m2v);
-	return err;
-}
-
-static GError *
-_resolve_m2_and_list(struct hc_resolver_s *r, struct hc_url_s *u,
-		GSList **beans)
-{
-	gchar **m2v = NULL;
-	GError *err;
-
-	if (NULL != (err = hc_resolve_reference_service(r, u, "meta2", &m2v))) {
-		g_prefix_error(&err, "Resolution error: ");
+	if (!rc) {
+		g_prefix_error(&err, "M2V1 error: ");
 		return err;
 	}
 
-	if (!*m2v)
-		err = NEWERROR(CODE_CONTAINER_NOTFOUND, "No meta2 located");
-	else
-		err =  _multiple_list_v2(m2v, u, beans);
-
-	g_strfreev(m2v);
-	return err;
+	*beans = m2v2_beans_from_raw_content_v2("fake", raw);
+	meta2_raw_content_v2_clean(raw);
+	return NULL;
 }
 
 
@@ -781,7 +678,10 @@ action_m2_list(struct http_request_s *rq, struct http_reply_ctx_s *rp,
 	// TODO manage snapshot ?
 
 	GSList *beans = NULL;
-	if (NULL != (err = _resolve_m2_and_list(resolver, url, &beans))) {
+	GError* hook(struct meta1_service_url_s *m2) {
+		return m2v2_remote_execute_LIST(m2->host, NULL, url, 0, &beans);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
 		hc_url_clean(url);
 		g_prefix_error(&err, "M2 error: ");
 		return _reply_soft_error(rp, err);
@@ -817,7 +717,15 @@ action_m2_get(struct http_request_s *rq, struct http_reply_ctx_s *rp,
 	}
 
 	GSList *beans = NULL;
-	if (NULL != (err = _resolve_m2_and_beans(resolver, url, &beans))) {
+	GError* hook(struct meta1_service_url_s *m2) {
+		GError *e = m2v2_remote_execute_GET(m2->host, NULL, url, 0, &beans);
+		if (!e || e->code == 404)
+			return e;
+		GRID_INFO("M2V2 error : probably a M2V1");
+		g_clear_error(&e);
+		return _single_get_v1(m2, url, &beans);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
 		hc_url_clean(url);
 		g_prefix_error(&err, "M2 error: ");
 		return _reply_soft_error(rp, err);
@@ -848,8 +756,16 @@ action_m2_beans(struct http_request_s *rq, struct http_reply_ctx_s *rp,
 		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
 	}
 
+	gchar *end = NULL;
+	const gchar *strpol = hc_url_get_option_value(url, "policy");
+	const gchar *strsize = hc_url_get_option_value(url, "size");
+	gint64 size = g_ascii_strtoll(strsize, &end, 10);
+
 	GSList *beans = NULL;
-	if (NULL != (err = _resolve_m2_and_get(resolver, url, &beans))) {
+	GError* hook (struct meta1_service_url_s *m2) {
+		return m2v2_remote_execute_BEANS(m2->host, NULL, url, strpol, size, 0, &beans);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
 		hc_url_clean(url);
 		g_prefix_error(&err, "M2 error: ");
 		return _reply_soft_error(rp, err);
@@ -862,6 +778,232 @@ action_m2_beans(struct http_request_s *rq, struct http_reply_ctx_s *rp,
 	return _reply_success_json(rp, gstr);
 }
 
+static enum http_rc_e
+action_m2_create(struct http_request_s *rq, struct http_reply_ctx_s *rp,
+		const gchar *uri)
+{
+	struct hc_url_s *url = NULL;
+	GError *err;
+
+	if (g_ascii_strcasecmp(rq->cmd, "POST"))
+		return _reply_method_error(rp);
+	if (NULL != (err = _extract_m2_url(uri, &url)))
+		return _reply_format_error(rp, err);
+
+	if (!hc_url_has(url, HCURL_NS) || !hc_url_has(url, HCURL_REFERENCE)) {
+		hc_url_clean(url);
+		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
+	}
+
+	GError* hook (struct meta1_service_url_s *m2) {
+		struct m2v2_create_params_s param = {
+			hc_url_get_option_value(url, "stgpol"),
+			hc_url_get_option_value(url, "verpol"),
+		};
+		return m2v2_remote_execute_CREATE(m2->host, NULL, url, &param);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
+		hc_url_clean(url);
+		g_prefix_error(&err, "M2 error: ");
+		return _reply_soft_error(rp, err);
+	}
+
+	return _reply_success_json(rp, NULL);
+}
+
+static enum http_rc_e
+action_m2_destroy(struct http_request_s *rq, struct http_reply_ctx_s *rp,
+		const gchar *uri)
+{
+	struct hc_url_s *url = NULL;
+	GError *err;
+
+	if (g_ascii_strcasecmp(rq->cmd, "POST"))
+		return _reply_method_error(rp);
+	if (NULL != (err = _extract_m2_url(uri, &url)))
+		return _reply_format_error(rp, err);
+
+	if (!hc_url_has(url, HCURL_NS) || !hc_url_has(url, HCURL_REFERENCE)) {
+		hc_url_clean(url);
+		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
+	}
+
+	GError* hook (struct meta1_service_url_s *m2) {
+		return m2v2_remote_execute_DESTROY(m2->host, NULL, url, 0);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
+		hc_url_clean(url);
+		g_prefix_error(&err, "M2 error: ");
+		return _reply_soft_error(rp, err);
+	}
+
+	return _reply_success_json(rp, NULL);
+}
+
+static enum http_rc_e
+action_m2_open(struct http_request_s *rq, struct http_reply_ctx_s *rp,
+		const gchar *uri)
+{
+	struct hc_url_s *url = NULL;
+	GError *err;
+
+	if (g_ascii_strcasecmp(rq->cmd, "POST"))
+		return _reply_method_error(rp);
+	if (NULL != (err = _extract_m2_url(uri, &url)))
+		return _reply_format_error(rp, err);
+
+	if (!hc_url_has(url, HCURL_NS) || !hc_url_has(url, HCURL_REFERENCE)) {
+		hc_url_clean(url);
+		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
+	}
+
+	GError* hook (struct meta1_service_url_s *m2) {
+		return m2v2_remote_execute_OPEN(m2->host, NULL, url);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
+		hc_url_clean(url);
+		g_prefix_error(&err, "M2 error: ");
+		return _reply_soft_error(rp, err);
+	}
+
+	return _reply_success_json(rp, NULL);
+}
+
+static enum http_rc_e
+action_m2_close(struct http_request_s *rq, struct http_reply_ctx_s *rp,
+		const gchar *uri)
+{
+	struct hc_url_s *url = NULL;
+	GError *err;
+
+	if (g_ascii_strcasecmp(rq->cmd, "POST"))
+		return _reply_method_error(rp);
+	if (NULL != (err = _extract_m2_url(uri, &url)))
+		return _reply_format_error(rp, err);
+
+	if (!hc_url_has(url, HCURL_NS) || !hc_url_has(url, HCURL_REFERENCE)) {
+		hc_url_clean(url);
+		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
+	}
+
+	GError* hook (struct meta1_service_url_s *m2) {
+		return m2v2_remote_execute_CLOSE(m2->host, NULL, url);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
+		hc_url_clean(url);
+		g_prefix_error(&err, "M2 error: ");
+		return _reply_soft_error(rp, err);
+	}
+
+	return _reply_success_json(rp, NULL);
+}
+
+static enum http_rc_e
+action_m2_has(struct http_request_s *rq, struct http_reply_ctx_s *rp,
+		const gchar *uri)
+{
+	struct hc_url_s *url = NULL;
+	GError *err;
+
+	if (g_ascii_strcasecmp(rq->cmd, "GET"))
+		return _reply_method_error(rp);
+	if (NULL != (err = _extract_m2_url(uri, &url)))
+		return _reply_format_error(rp, err);
+
+	if (!hc_url_has(url, HCURL_NS) || !hc_url_has(url, HCURL_REFERENCE)) {
+		hc_url_clean(url);
+		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
+	}
+
+	GError* hook (struct meta1_service_url_s *m2) {
+		return m2v2_remote_execute_HAS(m2->host, NULL, url);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
+		hc_url_clean(url);
+		g_prefix_error(&err, "M2 error: ");
+		return _reply_soft_error(rp, err);
+	}
+
+	return _reply_success_json(rp, NULL);
+}
+
+static enum http_rc_e
+action_m2_dedup(struct http_request_s *rq, struct http_reply_ctx_s *rp,
+		const gchar *uri)
+{
+	struct hc_url_s *url = NULL;
+	GError *err;
+
+	if (g_ascii_strcasecmp(rq->cmd, "POST"))
+		return _reply_method_error(rp);
+	if (NULL != (err = _extract_m2_url(uri, &url)))
+		return _reply_format_error(rp, err);
+
+	if (!hc_url_has(url, HCURL_NS) || !hc_url_has(url, HCURL_REFERENCE)) {
+		hc_url_clean(url);
+		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
+	}
+
+	gboolean first = TRUE;
+	GString *gstr = g_string_new("{\"\":[");
+	GError* hook (struct meta1_service_url_s *m2) {
+		gchar *msg = NULL;
+		GError *e = m2v2_remote_execute_DEDUP(m2->host, NULL, url, 0, &msg);
+		if (msg) {
+			if (first) {
+				g_string_append_c(gstr, ',');
+				first = FALSE;
+			}
+			g_string_append_c(gstr, '"');
+			g_string_append(gstr, msg); // TODO escpae this!
+			g_string_append_c(gstr, '"');
+		}
+		return e;
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
+		g_string_free(gstr, TRUE);
+		hc_url_clean(url);
+		g_prefix_error(&err, "M2 error: ");
+		return _reply_soft_error(rp, err);
+	}
+
+	g_string_append(gstr, "]}");
+	return _reply_success_json(rp, gstr);
+}
+
+static enum http_rc_e
+action_m2_purge(struct http_request_s *rq, struct http_reply_ctx_s *rp,
+		const gchar *uri)
+{
+	struct hc_url_s *url = NULL;
+	GError *err;
+
+	if (g_ascii_strcasecmp(rq->cmd, "POST"))
+		return _reply_method_error(rp);
+	if (NULL != (err = _extract_m2_url(uri, &url)))
+		return _reply_format_error(rp, err);
+
+	if (!hc_url_has(url, HCURL_NS) || !hc_url_has(url, HCURL_REFERENCE)) {
+		hc_url_clean(url);
+		return _reply_format_error(rp, NEWERROR(400, "Partial URL"));
+	}
+
+	GSList *beans = NULL;
+	GError* hook (struct meta1_service_url_s *m2) {
+		return m2v2_remote_execute_PURGE(m2->host, NULL, url, FALSE, 10.0, 10.0, &beans);
+	}
+	if (NULL != (err = _resolve_m2_and_do(resolver, url, hook))) {
+		hc_url_clean(url);
+		g_prefix_error(&err, "M2 error: ");
+		return _reply_soft_error(rp, err);
+	}
+
+	GString *gstr = g_string_sized_new(512);
+	_json_dump_all_beans(gstr, url, beans);
+	_bean_cleanl2(beans);
+	hc_url_clean(url);
+	return _reply_success_json(rp, gstr);
+}
 
 // Load-Balancing handler (stateless) ------------------------------------------
 
@@ -1221,9 +1363,18 @@ struct action_s
 			struct http_reply_ctx_s *rp, const gchar *uri);
 } actions[] = {
 	{"lb/sl/",               action_lb_sl},
+
 	{"m2/get/",              action_m2_get},
 	{"m2/beans/",            action_m2_beans},
 	{"m2/list/",             action_m2_list},
+	{"m2/create/",           action_m2_create},
+	{"m2/destroy/",          action_m2_destroy},
+	{"m2/open/",             action_m2_open},
+	{"m2/close/",            action_m2_close},
+	{"m2/has/",              action_m2_has},
+	{"m2/purge/",            action_m2_purge},
+	{"m2/dedup/",            action_m2_dedup},
+
 	{"dir/list/",            action_dir_list},
 	{"dir/link/",            action_dir_link},
 	{"dir/unlink/",          action_dir_unlink},
